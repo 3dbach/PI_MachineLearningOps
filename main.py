a5b09be44ee713ee65522eb65a6c3fcaabf2a32a
3dbach <121684211+3dbach@users.noreply.github.com>
2023-10-26 03:34:30 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -35,35 +35,7 @@ def developer(desarrollador: str):
     
     return result
 
-# Suponemos que ya has cargado tu DataFrame en una variable 'df'
-"""
-@app.get("/developer/{desarrollador}")
-def developer(desarrollador: str):
-    # Aquí iría el código para procesar y devolver los datos según el desarrollador.
-    #def developer(desarrollador: str) -> pd.DataFrame:
-    # Filtrar el DataFrame para el desarrollador especificado
-    developer_df = steam_games[steam_games['developer'] == desarrollador]
-    
-    # Agrupar por año
-    grouped = developer_df.groupby('year')
-    
-    # Contar la cantidad total de juegos por año
-    total_games = grouped.size()
-    
-    # Contar la cantidad de juegos que son "Free to Play" o "Free To Play" por año
-    free_games = developer_df[developer_df['price'].isin(['Free to Play', 'Free To Play'])].groupby('year').size()
-    
-    # Crear un DataFrame con los resultados
-    result = pd.DataFrame({
-        'Año': total_games.index,
-        'Cantidad de Items': total_games.values,
-        'Contenido Free': (free_games / total_games * 100).fillna(0).round(2)
-    }).reset_index(drop=True)
-    
-    return result
-"""
-# Test de la función con un ejemplo
-#developer("Kotoshiro")
+
 
 
 @app.get("/userdata/{User_id}")
